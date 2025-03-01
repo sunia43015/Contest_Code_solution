ac6008b128777aa5d9af00880fca78d1cbb4f916
sunia43015 <suniaakther43015@gmail.com>
2025-03-02 00:06:36 +0600

    contest_solve

---
+++ C++/joba.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--){
+    int n, m;
+    cin >> n >> m;
+    if (n * 5 >= m)
+    {
+        cout << "YES" << '\n';
+    }
+    else
+    {
+        cout << "NO" << '\n';
+    }
+}
+
+    return 0;
+}
---
+++ C++/lili.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+int n;
+cin>>n;
+cout<<pow(n,3)<<'\n';
+
+    return 0;
+}
